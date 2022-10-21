7ad58cd22b05355e402c289a95386cb8d762cd38
Yazid <bentaib.mohammedyazid@univ-bechar.dz>
2022-10-21 09:19:53 +0100

    show data fixed
    getting data and showing them is working fine + changed some variables names

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from unittest import result
-from simplexe import *
+from simplex import *
 
 objective_function=[]
 constraints=[]
@@ -10,13 +10,13 @@ N_Variables=Get_Data("How many variables are there? *ps only accept 2 or more*\n
 
 N_constraints=Get_Data("How many constraints are there?\n")
 
-objective_function=Fill_Tables(N_Variables,"Enter the coefficient of the variable X%s of the objective function :","Objective_or_results")
+objective_function=Fill_Tables(N_Variables,0,"Enter the coefficient of the variable X%s of the objective function :","Objective")
 
-constraints=Fill_Tables(N_constraints,"Enter the coefficient of the variable X%s of the constraint number %s :","Constraints")
+constraints=Fill_Tables(N_Variables,N_constraints,"Enter the coefficient of the variable X%s of the constraint number %s :","Constraints")
 
-results=Fill_Tables(N_constraints,"Enter the result of the constraint number %s :","Objective_or_results")
+results=Fill_Tables(N_Variables,N_constraints,"Enter the result of the constraint number %s :","results")
 print("====================\n")
-Show_Data(objective_function,constraints,results,N_Variables)
+Show_Data(objective_function,constraints,results,N_Variables,)
 
 # print(objective_function)
 # print(constraints)
--- simplex.py
+++ simplex.py
@@ -14,19 +14,27 @@ def Get_Data(Message):
     #         constraints.append(input("Enter the coefficient of the variable X"+str(j+1)+" of the constraint number "+str(i+1)+" :"))
     #     results.append(input("Enter the result of the constraint number "+str(i+1)+" :"))
     
-def Fill_Tables(Number,Message,Table_Type):
-    Table=[];j=0
-    for i in range(0,Number):
-        if Table_Type=="Objective_or_results":
+def Fill_Tables(Var_Number,Const_Number,Message,Table_Type):
+    Table=[];index=0;counter=1
+    if Table_Type=="Objective":
+        for i in range(0,Var_Number):
+                Table.append(input(Message%str(i+1)))
+    elif Table_Type=="Constraints":
+        for i in range(0,Var_Number):
+            for j in range (0,Const_Number):
+                Table.append(input(Message%(str(index+1),str(counter))))
+                index+=1;
+                if index==Var_Number:
+                    index=0
+                    counter+=1
+    elif Table_Type=="results":
+        for i in range(0,Const_Number):
             Table.append(input(Message%str(i+1)))
-        elif Table_Type=="Constraints":
-            Table.append(input(Message%(str(i+1),str(j+1))))
-            j+=1
     print("____________________\n")
     return Table
 
-def Show_Data(objective_function,constraints,results,N_Variables):
-    Result_Index=0
+def Show_Data(objective_function,constraints,results,Var_Number):
+    Result_Index=0;index=0
     print("Objective function :",end=" ")
     print("Max Z = ",end="")
     for i in range(0,len(objective_function)):
@@ -35,11 +43,14 @@ def Show_Data(objective_function,constraints,results,N_Variables):
             print(" + ",end="")
     print("\n====================\nConstraints :\n")
     for i in range(0,len(constraints)):
-        print(str(constraints[i])+" X"+str(i+1),end="")
-        if((i+1)%N_Variables==0):
+        index+=1;
+        print(str(constraints[i])+" X"+str(index),end="")
+        if((i+1)%Var_Number==0):
             print(" <= "+str(results[Result_Index])+"\n")
             Result_Index+=1
         else:
             print(" + ",end="")
+        if index==Var_Number:
+                    index=0
 # Get_Data()
 # Show_Data()
