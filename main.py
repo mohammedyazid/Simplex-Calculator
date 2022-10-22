90f8fb4467f5110d4ffca7a1808fa5ed04d40bd6
Yazid <bentaib.mohammedyazid@univ-bechar.dz>
2022-10-22 11:16:13 +0100

    Variables control added

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
-from unittest import result
 from simplex import *
 
-objective_function=[]
-constraints=[]
-results=[]
+Obj_Table=[]
+Const_Table=[]
+Results=[]
+Eq_Type=[]
+Tms=[]
+Constraints=[]
 
 
-N_Variables=Get_Data("How many variables are there? *ps only accept 2 or more*\n")
-
-N_constraints=Get_Data("How many constraints are there?\n")
-
-objective_function=Fill_Tables(N_Variables,0,"Enter the coefficient of the variable X%s of the objective function :","Objective")
-
-constraints=Fill_Tables(N_Variables,N_constraints,"Enter the coefficient of the variable X%s of the constraint number %s :","Constraints")
-
-results=Fill_Tables(N_Variables,N_constraints,"Enter the result of the constraint number %s :","results")
-print("====================\n")
-Show_Data(objective_function,constraints,results,N_Variables,)
-
-# print(objective_function)
-# print(constraints)
-# print(results)
-# Show_Data()
+N_Variables=Get_Data("How many variables are there?\n",2,20)
+N_constraints=Get_Data("How many constraints are there?\n",1,20)
+Obj_Table,Const_Table,Results,Eq_Type=Fill_Tables(N_Variables,N_constraints,Obj_Table,Const_Table,Results,Eq_Type)
+Show_Data(N_Variables,Obj_Table,Const_Table,Results,Eq_Type)
--- simplex.py
+++ simplex.py
@@ -1,56 +1,55 @@
-# objective_function=[5,4]
-# constraints=[1,3,5,8,9,10]
-# results=[25,83,50]
-# N_Variables=2
-
-def Get_Data(Message):
-    Number=int(input(Message))
+from simple_term_menu import TerminalMenu
+options = ['=', '>=', '<=']
+def Get_Data(Message,min,max):
+    while True:
+        try:
+            Number=int(input(Message))
+            if Number<min or Number>max:
+                raise ValueError
+            break
+        except ValueError:
+            print("______________________________!!!!!!!______________________________")
+            print("Invalid input. Please enter a number between %s and %s."%(min,max))
+            print("______________________________!!!!!!!______________________________\n")
     return Number
-    # N_Variables=int(input("How many variables are there? *ps only accept 2 or more*\n"))
-    # for i in range(0,N_Variables):
-    #     objective_function.append(input(f"Enter the coefficient of the variable {str(i+1)} of the objective function :"))
-    # for i in range(0,N_constraint):
-    #     for j in range(0,N_Variables):
-    #         constraints.append(input("Enter the coefficient of the variable X"+str(j+1)+" of the constraint number "+str(i+1)+" :"))
-    #     results.append(input("Enter the result of the constraint number "+str(i+1)+" :"))
-    
-def Fill_Tables(Var_Number,Const_Number,Message,Table_Type):
-    Table=[];index=0;counter=1
-    if Table_Type=="Objective":
-        for i in range(0,Var_Number):
-                Table.append(input(Message%str(i+1)))
-    elif Table_Type=="Constraints":
-        for i in range(0,Var_Number):
-            for j in range (0,Const_Number):
-                Table.append(input(Message%(str(index+1),str(counter))))
-                index+=1;
-                if index==Var_Number:
-                    index=0
-                    counter+=1
-    elif Table_Type=="results":
-        for i in range(0,Const_Number):
-            Table.append(input(Message%str(i+1)))
-    print("____________________\n")
-    return Table
 
-def Show_Data(objective_function,constraints,results,Var_Number):
+def Fill_Tables(Var_Number,Const_Number,Obj_Table,Const_Table,Results,Eq_Type):
+    index=0;counter=0
+    for i in range(0,Var_Number):
+        Obj_Table.append(input(f"Enter the coefficient of the variable X{str(i+1)} of the objective function: "))
+    
+    for i in range(0,Var_Number):
+        print(f"\n|_________________________________{counter+1}_________________________________|\n")
+        for j in range (0,Const_Number):
+            Const_Table.append(input(f"Enter the coefficient of the variable X{str(index+1)} of the constraint number {str(counter+1)}: "))
+            index+=1;
+            if index==Var_Number:
+                index=0
+                terminal_menu = TerminalMenu(options,title=f"Select the type of the constraint number: {str(counter+1)}")
+                menu_entry_index = terminal_menu.show()
+                Eq_Type.append(options[menu_entry_index])
+                Results.append(input(f"Enter the result of the constraint number {str(i+1)}:"))
+                counter+=1
+                print(f"\n|_________________________________{counter+1}_________________________________|\n")
+    return Obj_Table,Const_Table,Results,Eq_Type
+
+def Show_Data(Var_Number,Obj_Table,Const_Table,Results,Eq_Type):
     Result_Index=0;index=0
-    print("Objective function :",end=" ")
+    print("\n========================Objective Function========================")
     print("Max Z = ",end="")
-    for i in range(0,len(objective_function)):
-        print(str(objective_function[i])+" X"+str(i+1),end="")
-        if(i<len(objective_function)-1):
+    for i in range(0,len(Obj_Table)):
+        print(str(Obj_Table[i])+" X"+str(i+1),end="")
+        if(i<len(Obj_Table)-1):
             print(" + ",end="")
-    print("\n====================\nConstraints :\n")
-    for i in range(0,len(constraints)):
+    print("\n____________________________Constraints____________________________\n")
+    for i in range(0,len(Const_Table)):
         index+=1;
-        print(str(constraints[i])+" X"+str(index),end="")
+        print(str(Const_Table[i])+" X"+str(index) ,end="")
         if((i+1)%Var_Number==0):
-            print(" <= "+str(results[Result_Index])+"\n")
+            print(Eq_Type[Result_Index] + str(Results[Result_Index])+"\n")
             Result_Index+=1
         else:
             print(" + ",end="")
         if index==Var_Number:
                     index=0
-# Get_Data()
-# Show_Data()
+    
